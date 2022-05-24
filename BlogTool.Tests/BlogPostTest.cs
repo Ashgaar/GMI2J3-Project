7a672f09fad83da09343210923dfdd31def08bdb
Mikael Olsson <zangiloxd@hotmail.com>
2022-05-24 15:10:16 +0200

    Added test for blogpost tostring.

---
+++ BlogTool.Tests/BlogPostTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BlogTool.Tests
+{
+    public class BlogPostTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void TestToString()
+        {
+            BlogPost blogPost = new BlogPost();
+            blogPost.Title = "test";
+            blogPost.Content = "testtest";
+            blogPost.Date = DateTime.Parse("2020-01-01");
+            Assert.AreEqual("Datum: 2020-01-01 Tid: 12:00\nRubrik: test\ntesttest\n", blogPost.ToString());
+        }
+    }
+}
+
